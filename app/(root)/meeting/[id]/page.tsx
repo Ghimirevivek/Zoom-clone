3fb65adc559489cf4c3271e831f43e0ba18377cf
Vivek Ghimire <108508449+Ghimirevivek@users.noreply.github.com>
2024-10-29 19:12:42 +0530

    fourth  commit

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -3,24 +3,18 @@
 import { useState } from 'react';
 import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-
+import { useParams } from 'next/navigation';
 import { Loader } from 'lucide-react';
 
 import { useGetCallById } from '@/hooks/useGetCallById';
-// import Alert from '@/components/Alert';
+import Alert from '@/components/Alert';
 import MeetingSetup from '@/components/MeetingSetup';
 import MeetingRoom from '@/components/MeetingRoom';
 
-interface Params {
-  params: {
-    id: string;
-  };
-}
-
-const MeetingPage = ({ params }: Params) => {
-  // const { id } = useParams();
+const MeetingPage = () => {
+  const { id } = useParams();
   const { isLoaded, user } = useUser();
-  const { call, isCallLoading } = useGetCallById(params.id);
+  const { call, isCallLoading } = useGetCallById(id?? "");
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   if (!isLoaded || isCallLoading) return <Loader />;
@@ -32,9 +26,9 @@ const MeetingPage = ({ params }: Params) => {
   );
 
   // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
-  // const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
 
-  // if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
